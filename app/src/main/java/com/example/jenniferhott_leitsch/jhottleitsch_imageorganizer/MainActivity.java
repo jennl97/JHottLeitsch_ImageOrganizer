38fab171e8adb10fb7017570cf2959a32950065c
jennl97 <jennhott97@gmail.com>
2018-03-17 16:08:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jenniferhott_leitsch/jhottleitsch_imageorganizer/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.jenniferhott_leitsch.jhottleitsch_imageorganizer;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Integer[] Georgia = {R.drawable.menthebench, R.drawable.remains, R.drawable.spider, R.drawable.storyteller, R.drawable.tabbyhouse, R.drawable.thebench, R.drawable.thesquare, R.drawable.wormslow};
+    ImageView pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView grid = (GridView) findViewById(R.id.gridView);
+        final ImageView pic = (ImageView) findViewById(R.id.imgLarge);
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "Savannah, Georgia" + (position +1), Toast.LENGTH_SHORT).show();
+                pic.setImageResource(Georgia[position]);
+            }
+        });
+
+    }
+
+    public class ImageAdapter extends BaseAdapter{
+        private Context context;
+        public ImageAdapter(Context c) { context = c; }
+
+        @Override
+        public int getCount() {
+            return Georgia.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            pic = new ImageView(context);
+            pic.setImageResource(Georgia[position]);
+            pic.setScaleType(ImageView.ScaleType.FIT_XY);
+            pic.setLayoutParams(new GridView.LayoutParams(330,300));
+            return pic;
+        }
+    }
+}
